a6b80f2600d7573e39223eaf4cda10ba5152cd07
iamanimesh11 <122896955+iamanimesh11@users.noreply.github.com>
2023-08-20 20:48:55 +0530

    Update ty.py

--- ty.py
+++ ty.py
@@ -1,55 +1,46 @@
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+import av
 import cv2
-import streamlit as st
 from deepface import DeepFace
 
-def main():
-    st.title("Emotion Detection with DeepFace and OpenCV")
+class EmotionVideoProcessor:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Load the Haarcascade classifier
-    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
+        gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, 1.1, 3)
 
-    # Initialize the video capture from webcam
-    cap = cv2.VideoCapture(0)
+        for x, y, w, h in faces:
+            try:
+                result = DeepFace.analyze(frm[y:y + h, x:x + w], actions=['emotion'])
+                dominant_emotion = result[0]['dominant_emotion']
 
-    # Check if the webcam is opened correctly
-    if not cap.isOpened():
-        raise IOError("Cannot open webcam")
+                emotion_text = f"Emotion: {dominant_emotion}"
+                font =  cv2.FONT_HERSHEY_SIMPLEX
+                # Add text shadow
+                shadow_offset = 2
+                shadow_color = (0, 0, 0)  # Black shadow color
+                cv2.putText(frm, emotion_text, (x + w + 10+shadow_offset, y + h // 2+shadow_offset), font, 0.8,shadow_color,  2, cv2.LINE_AA)
+            except ValueError as e:
+                error_message = "Face not detected :("
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frm, error_message, (x, y - 20), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
+                cv2.putText(frm, "Please check your position", (x, y + h + 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
-    while True:
-        ret, frame = cap.read()  # Read one frame from the video stream
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # Detect faces in the frame
-        faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+        return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
-        for (x, y, w, h) in faces:
-            # Draw a rectangle around the face
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+rtc_configuration = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
-            # Analyze emotions using DeepFace
-            result = DeepFace.analyze(frame[y:y + h, x:x + w], actions=['emotion'])
-
-            # Get the dominant emotion
-            dominant_emotion = result['dominant_emotion']
-
-            # Display the dominant emotion text beside the face
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(frame,
-                        dominant_emotion,
-                        (x + w + 10, y + h // 2),
-                        font, 0.5,
-                        (0, 0, 255),
-                        1,
-                        cv2.LINE_AA)
-
-        st.image(frame, channels="BGR", caption="Emotion Detection")
-
-        # Break the loop when 'q' is pressed
-        if st.button("Quit"):
-            break
-
-    # Release the video capture and close all windows
-    cap.release()
-
-if __name__ == "__main__":
-    main()
+webrtc_streamer(
+    key="emotion-detection",
+    video_processor_factory=EmotionVideoProcessor,
+    rtc_configuration=rtc_configuration,
+    # Remove the AudioProcessor
+    video_transformer_factory=None,
+)
