960e1c481c4302a4c61f5c7c4ed717b084f15c1b
iamanimesh11 <122896955+iamanimesh11@users.noreply.github.com>
2023-08-21 15:51:29 +0530

    Update ty.py

--- ty.py
+++ ty.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from streamlit_webrtc import VideoProcessorBase, webrtc_streamer
 
-# Define your video transformer class
-class EmotionVideoTransformer(VideoTransformerBase):
-   def transform(self, frame):
+# Define your video processor class
+class EmotionVideoProcessor(VideoProcessorBase):
+    def recv(self, frame):
         # Your emotion detection and processing logic here
         processed_frame = frame  # Process the frame as needed
         return processed_frame
@@ -11,14 +11,14 @@ class EmotionVideoTransformer(VideoTransformerBase):
 def main():
     st.title("Real-time Emotion Detection")
 
-    # Start the WebRTC component with your video transformer
+    # Start the WebRTC component with your video processor
     webrtc_ctx = webrtc_streamer(
         key="emotion-detection",
-        video_transformer_factory=EmotionVideoTransformer,
-        async_transform=True
+        video_processor_factory=EmotionVideoProcessor,
+        async_processing=True
     )
 
-    if not webrtc_ctx.video_transformer:
+    if not webrtc_ctx.video_processor:
         st.warning("Waiting for video to start...")
         return
 
