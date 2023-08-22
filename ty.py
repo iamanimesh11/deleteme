d334fc4ae7b8d158fad52e81b59d202b5ef5c441
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2023-08-23 03:38:38 +0530

    Update ty.py

--- ty.py
+++ ty.py
@@ -1,47 +1,33 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
-import av
-import cv2
-from deepface import DeepFace
+import gspread
+from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-class EmotionVideoProcessor(VideoProcessorBase):
-    def __init__(self):
-        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# Authenticate with Google Sheets using Streamlit secrets
+def authenticate_google_sheets():
+    creds = st.secrets["gcp_service_account"]
+    client = gspread.service_account_from_dict(creds)
+    return client
 
-    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-        frm = frame.to_ndarray(format="bgr24")
-        gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, 1.1, 3)
+# Main function
+def main():
+    st.title("Rating App")
 
-        for x, y, w, h in faces:
-            try:
-                result = DeepFace.analyze(frm[y:y + h, x:x + w], actions=['emotion'])
-                dominant_emotion = result[0]['dominant_emotion']
+    # Authenticate with Google Sheets
+    gs_client = authenticate_google_sheets()
+    if not gs_client:
+        st.warning("Authentication failed. Please check your credentials.")
+        return
 
-                emotion_text = f"Emotion: {dominant_emotion}"
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                shadow_offset = 2
-                shadow_color = (0, 0, 0)
-                cv2.putText(frm, emotion_text, (x + w + 10 + shadow_offset, y + h // 2 + shadow_offset), font, 0.8, shadow_color, 2, cv2.LINE_AA)
-            except ValueError as e:
-                error_message = "Face not detected :("
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(frm, error_message, (x, y - 20), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
-                cv2.putText(frm, "Please check your position", (x, y + h + 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+    # Rating input
+    rating = st.slider("Rate this app:", min_value=1, max_value=5, value=3)
 
-            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    if st.button("Submit"):
+        # Write rating to Google Sheet
+        sheet = gs_client.open("hello").sheet1
+        sheet.append_row([rating])
+        st.success("Rating submitted!")
 
-        return av.VideoFrame.from_ndarray(frm, format='bgr24')
-
-RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-)
-
-st.set_page_config(page_title="Streamlit WebRTC Emotion Detection", page_icon="🎭")
-
-webrtc_streamer(
-    key="emotion-detection",
-    video_processor_factory=EmotionVideoProcessor,
-    rtc_configuration=RTC_CONFIGURATION,
-    media_stream_constraints={"video": True, "audio": False}
-)
+if __name__ == "__main__":
+    main()
