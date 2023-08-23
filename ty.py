ce08798da67069339b226f59018b441b9772acdf
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2023-08-23 11:07:37 +0530

    Update ty.py

--- ty.py
+++ ty.py
@@ -1,33 +1,93 @@
 import streamlit as st
-import gspread
-from google.auth.transport.requests import Request
-from google.auth.exceptions import RefreshError
-from google_auth_oauthlib.flow import InstalledAppFlow
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
+import av
+import threading
+import cv2
+from deepface import DeepFace
 
-# Authenticate with Google Sheets using Streamlit secrets
-def authenticate_google_sheets():
-    creds = st.secrets["gcp_service_account"]
-    client = gspread.service_account_from_dict(creds)
-    return client
+class EmotionVideoProcessor(VideoProcessorBase):
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Main function
-def main():
-    st.title("Rating App")
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        frm = frame.to_ndarray(format="bgr24")
+        gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, 1.1, 3)
 
-    # Authenticate with Google Sheets
-    gs_client = authenticate_google_sheets()
-    if not gs_client:
-        st.warning("Authentication failed. Please check your credentials.")
-        return
+        for x, y, w, h in faces:
+            try:
+                result = DeepFace.analyze(frm[y:y + h, x:x + w], actions=['emotion'])
+                dominant_emotion = result[0]['dominant_emotion']
 
-    # Rating input
-    rating = st.slider("Rate this app:", min_value=1, max_value=5, value=3)
+                emotion_text = f"Emotion: {dominant_emotion}"
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                shadow_offset = 2
+                shadow_color = (0, 0, 0)
+                cv2.putText(frm, emotion_text, (x + w + 10 + shadow_offset, y + h // 2 + shadow_offset), font, 0.8, shadow_color, 2, cv2.LINE_AA)
+            except ValueError as e:
+                error_message = "Face not detected :("
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frm, error_message, (x, y - 20), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
+                cv2.putText(frm, "Please check your position", (x, y + h + 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
-    if st.button("Submit"):
-        # Write rating to Google Sheet
-        sheet = gs_client.open("https://docs.google.com/spreadsheets/d/1CedcZYzZ9kkslFg4yZjYsZLUKQB0jvCZ7atcBpOszd8/edit?usp=sharing").sheet1
-        sheet.append_row([rating])
-        st.success("Rating submitted!")
+            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-if __name__ == "__main__":
-    main()
+        return av.VideoFrame.from_ndarray(frm, format='bgr24')
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+st.set_page_config(page_title="Streamlit WebRTC Demo", page_icon="🤖")
+task_list = ["Video Stream"]
+
+with st.sidebar:
+    st.title('Task Selection')
+    task_name = st.selectbox("Select your tasks:", task_list)
+st.title(task_name)
+
+if task_name == task_list[0]:
+    style_list = ['color', 'black and white']
+
+    st.sidebar.header('Style Selection')
+    style_selection = st.sidebar.selectbox("Choose your style:", style_list)
+
+    class VideoProcessor(VideoProcessorBase):
+        def __init__(self):
+            self.model_lock = threading.Lock()
+            self.style = style_list[0]
+
+        def update_style(self, new_style):
+            if self.style != new_style:
+                with self.model_lock:
+                    self.style = new_style
+
+        def recv(self, frame):
+            # img = frame.to_ndarray(format="bgr24")
+            img = frame.to_image()
+            if self.style == style_list[1]:
+                img = img.convert("L")
+
+            # return av.VideoFrame.from_ndarray(img, format="bgr24")
+            return av.VideoFrame.from_image(img)
+
+    ctx = webrtc_streamer(
+        key="example",
+        video_processor_factory=VideoProcessor,
+        rtc_configuration=RTC_CONFIGURATION,
+        media_stream_constraints={
+            "video": True,
+            "audio": False
+        }
+    )
+
+    if ctx.video_processor:
+        ctx.video_transformer.update_style(style_selection)
+st.set_page_config(page_title="Streamlit WebRTC Emotion Detection", page_icon="🎭")
+
+webrtc_streamer(
+    key="emotion-detection",
+    video_processor_factory=EmotionVideoProcessor,
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False}
+)
